d4429da12a4db17b1e4f364b0c99e97d98546f1d
Arthur Badin <akherousin94@gmail.com>
2024-02-27 21:21:09 +0600

    feat: add Links component

---
+++ src/components/Links/ChevronRightSvg.tsx
@@ -0,0 +1,17 @@
+function ChevronRightSvg() {
+  return (
+    <svg
+      width="8"
+      height="12"
+      viewBox="0 0 8 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      focusable="false"
+    >
+      <path d="M1.3219 1L6.3219 6L1.3219 11" stroke="#D87D4A" strokeWidth="2" />
+    </svg>
+  );
+}
+
+export default ChevronRightSvg;
---
+++ src/components/Links/Links.tsx
@@ -0,0 +1,75 @@
+import paths from '@/paths';
+import styles from './Links.module.css';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import ChevronRightSvg from './ChevronRightSvg';
+
+import headphonesImg from '@/assets/shared/desktop/image-category-thumbnail-headphones.png';
+import speakersImg from '@/assets/shared/desktop/image-category-thumbnail-speakers.png';
+import earphonesImg from '@/assets/shared/desktop/image-category-thumbnail-earphones.png';
+
+function Links() {
+  return (
+    <ul className={styles.list}>
+      <li className={`${styles.item} | box`}>
+        <h2 className="h6">Headphones</h2>
+        <Link
+          href={paths.showCategoryPage('headphones')}
+          className={`${styles.link} | subtitle`}
+        >
+          <span>
+            Shop <span className="visually-hidden">for Headphones</span>
+          </span>
+          <ChevronRightSvg />
+        </Link>
+        <div
+          className={`${styles.image}  | image-wrapper`}
+          data-image="headphones"
+        >
+          <Image alt="" src={headphonesImg} />
+        </div>
+      </li>
+
+      <li className={`${styles.item} | box`}>
+        <h2 className="h6">Speakers</h2>
+        <Link
+          href={paths.showCategoryPage('speakers')}
+          className={`${styles.link} | subtitle`}
+        >
+          <span>
+            Shop <span className="visually-hidden">for Speakers</span>
+          </span>
+          <ChevronRightSvg />
+        </Link>
+        <div
+          className={`${styles.image}  | image-wrapper`}
+          data-image="speakers"
+        >
+          <Image alt="" src={speakersImg} />
+        </div>
+      </li>
+
+      <li className={`${styles.item} | box`}>
+        <h2 className="h6">Earphones</h2>
+        <Link
+          href={paths.showCategoryPage('earphones')}
+          className={`${styles.link} | subtitle`}
+        >
+          <span>
+            Shop <span className="visually-hidden">for Earphones</span>
+          </span>
+          <ChevronRightSvg />
+        </Link>
+        <div
+          className={`${styles.image}  | image-wrapper`}
+          data-image="earphones"
+        >
+          <Image alt="" src={earphonesImg} />
+        </div>
+      </li>
+    </ul>
+  );
+}
+
+export default Links;
