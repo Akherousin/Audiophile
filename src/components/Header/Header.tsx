63de4667f2c725f19b70828434a8284e9c820f4b
Arthur Badin <akherousin94@gmail.com>
2024-02-27 21:22:30 +0600

    feat: add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,61 @@
+import paths from '@/paths';
+import styles from './Header.module.css';
+
+import Logo from '@/components/Logo';
+import NavLink from '@/components/NavLink';
+
+import CartSvg from './CartSvg';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className="wrapper">
+        <Logo />
+
+        <nav>
+          <ul className={'navlinks'}>
+            <li>
+              <NavLink
+                href={paths.home()}
+                className={`navlink subtitle click-target-helper`}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                href={paths.showCategoryPage('headphones')}
+                className={`navlink subtitle click-target-helper`}
+              >
+                Headphones
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                href={paths.showCategoryPage('speakers')}
+                className={`navlink subtitle click-target-helper`}
+              >
+                Speakers
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                href={paths.showCategoryPage('earphones')}
+                className={`navlink subtitle click-target-helper`}
+              >
+                Earphones
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <button className={`${styles.cart} | click-target-helper`}>
+          <CartSvg />
+          <span className="visually-hidden">Cart</span>
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
