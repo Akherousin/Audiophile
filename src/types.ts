0d56822e1e6d4b65eaa479b424335821a175f2fc
Arthur Badin <akherousin94@gmail.com>
2024-02-28 21:23:04 +0600

    feat: add common types

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export type Category = 'headphones' | 'speakers' | 'earphones';
+
+export type Product = {
+  id: number;
+  slug: string;
+  name: string;
+  image: {
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  category: Category;
+  categoryImage: {
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  isNew: boolean;
+  price: number;
+  description: string;
+  features: string;
+  includes: { quantity: number; item: string }[];
+  gallery: {
+    first: { mobile: string; tablet: string; desktop: string };
+    second: { mobile: string; tablet: string; desktop: string };
+    third: { mobile: string; tablet: string; desktop: string };
+  };
+  others: Product[];
+};
