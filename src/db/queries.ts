36524a4f196c9f164f91a357c7201f6aecef2435
Arthur Badin <akherousin94@gmail.com>
2024-02-28 21:26:50 +0600

    feat: add data and query logic

---
+++ src/db/queries.ts
@@ -0,0 +1,37 @@
+import { promises as fs } from 'fs';
+import { type Product, type Category } from '@/types';
+
+export const fetchAllProducts = async (): Promise<Product[]> => {
+  const data = await fs.readFile(process.cwd() + '/src/db/data.json', 'utf8');
+  const products = JSON.parse(data);
+
+  return products;
+};
+
+export const fetchProductsByCategory = async (category: Category) => {
+  const products = await fetchAllProducts();
+  const filteredProducts = products
+    .filter((product) => product.category === category)
+    .map(({ id, slug, name, description, image, isNew }) => {
+      return {
+        id,
+        slug,
+        name,
+        description,
+        image,
+        isNew,
+      };
+    })
+    .reverse();
+
+  return filteredProducts;
+};
+
+export const fetchProductBySlug = async (
+  slug: string
+): Promise<Product | null> => {
+  const products = await fetchAllProducts();
+  let product = products.find((product) => product.slug == slug) || null;
+
+  return product;
+};
