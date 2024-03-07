2c27b58f8a321c35f110b3ca23c22cdaf154eb29
Arthur Badin <akherousin94@gmail.com>
2024-03-08 03:00:07 +0600

    refactor: simplify link

--- src/components/GoBackLink/GoBackLink.tsx
+++ src/components/GoBackLink/GoBackLink.tsx
@@ -1,34 +1,18 @@
 'use client';
 
-import paths from '@/paths';
 import styles from './GoBackLink.module.css';
-
-import { useRouter, useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
 function GoBackLink() {
   const router = useRouter();
-  const searchParams = useSearchParams();
-  let isFirstVisit = false;
-
-  if (typeof window !== 'undefined') {
-    isFirstVisit = sessionStorage.getItem('isFirstVisit') === null;
-  }
-  const goToHomePage = searchParams.get('updated') === 'true';
 
   return (
     <Link
       href="#"
       onClick={(e) => {
         e.preventDefault();
-        sessionStorage.setItem('isFirstVisit', 'false');
-        if (isFirstVisit) return router.push(paths.home());
-        if (goToHomePage) {
-          return router.push(paths.home());
-        } else {
-          return router.back();
-        }
+        router.back();
       }}
       className={`${styles.link} | opaque click-target-helper`}
     >
@@ -37,12 +21,4 @@ function GoBackLink() {
   );
 }
 
-function SuspensedGoBackLink() {
-  return (
-    <Suspense fallback={null}>
-      <GoBackLink />
-    </Suspense>
-  );
-}
-
-export default SuspensedGoBackLink;
+export default GoBackLink;
